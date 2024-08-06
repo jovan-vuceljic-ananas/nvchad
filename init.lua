30182a85d47e03c8e121844d8afb18fef4c1d5e9
Jovan Vuceljic <jovan.vuceljic@ananas.rs>
2024-08-06 02:07:41 +0200

    customs

--- init.lua
+++ init.lua
@@ -2,10 +2,11 @@ require "core"
 
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-
 if custom_init_path then
   dofile(custom_init_path)
 end
+
+-- custom config
 if vim.g.neovide then
   vim.g.neovide_transparency = 0.9
   vim.g.neovide_scale_factor = 0.8
@@ -19,9 +20,11 @@ if vim.g.neovide then
   vim.g.neovide_show_border = true
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_fullscreen = true
-  vim.o.guifont='Hack Nerd Font'
+  vim.o.guifont = 'Hack Nerd Font'
 end
-
+vim.opt.relativenumber = true
+-- end of custom
+--
 require("core.utils").load_mappings()
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
